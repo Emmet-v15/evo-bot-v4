d6ea826ec20149254614d4f7cbe626c776c0e4d8
Emmet_v15 <emmettrey726@gmail.com>
2023-01-30 20:47:44 +0000

    update

--- commands/slash/settings/get.js
+++ commands/slash/settings/get.js
@@ -5,13 +5,11 @@ module.exports = {
     execute: async (/** @type {require("discord.js").Client} */ client, /** @type {require("discord.js").CommandInteraction} */ interaction) => {
         await interaction.deferReply({ ephemeral: false });
         const key = interaction.options.getString("key");
-        if (user) {
-            console.log("abc");
+        console.log("abc");
 
-            if (!client.settings.has(interaction.guild.id, key)) return interaction.editReply({ content: "This key does not exist in the current guild." });
-            const value = client.settings.get(interaction.guild.id, key);
-            interaction.editReply({ content: `The value for \`${key}\` is \`${value}\`` });
-        }
+        if (!client.settings.has(interaction.guild.id, key)) return interaction.editReply({ content: "This key does not exist in the current guild." });
+        const value = client.settings.get(interaction.guild.id, key);
+        interaction.editReply({ content: `The value for \`${key}\` is \`${value}\`` });
     },
     options: [
         {
