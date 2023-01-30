3e58ad8dc0f317da70df2ed8404c310fc8c91fa8
Emmet_v15 <emmettrey726@gmail.com>
2023-01-30 20:02:45 +0000

    update

---
+++ commands/slash/permission/set.js
@@ -0,0 +1,35 @@
+module.exports = {
+    name: "set",
+    description: "Set permission level for a user.",
+    permission: 2,
+    execute: async (/** @type {require("discord.js").Client} */ client, /** @type {require("discord.js").CommandInteraction} */ interaction) => {
+        interaction.deferReply({ content: "Placeholder command." });
+        const user = interaction.options.getUser("user");
+        const permission = interaction.options.getInteger("permission");
+        if (user) {
+            const id = user.id;
+            // set permission for user to permission in the database
+
+            // check if permission is valid
+            if (permission < 0 || permission > 2) return interaction.editReply({ content: "Invalid permission level. [1-2]" });
+
+            client.settings.set(interaction.guild.id, permission, `permissions.${id}`);
+
+            interaction.editReply({ content: `The permission for <@${id}> has been set to \`${permission}\`` });
+        }
+    },
+    options: [
+        {
+            type: "User",
+            name: "user",
+            description: "The user to set the permission for.",
+            required: true,
+        },
+        {
+            type: "Integer",
+            name: "permission",
+            description: "The permission level to set.",
+            required: true,
+        },
+    ],
+};
