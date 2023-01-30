303821bda2fe41b174b3f355394718561d5cfe87
Emmet_v15 <emmettrey726@gmail.com>
2023-01-30 20:47:34 +0000

    update

--- commands/slash/settings/get.js
+++ commands/slash/settings/get.js
@@ -6,9 +6,10 @@ module.exports = {
         await interaction.deferReply({ ephemeral: false });
         const key = interaction.options.getString("key");
         if (user) {
+            console.log(value);
+
             if (!client.settings.has(interaction.guild.id, key)) return interaction.editReply({ content: "This key does not exist in the current guild." });
             const value = client.settings.get(interaction.guild.id, key);
-            console.log(value);
             interaction.editReply({ content: `The value for \`${key}\` is \`${value}\`` });
         }
     },
