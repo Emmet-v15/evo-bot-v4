9cd2e0c789b771469d05fab02a0b70742bef7d97
emmet-v15 <emmet@v15.studio>
2023-03-05 05:28:40 +0000

    upgrading ticket system

--- commands/slash/giveaway/premium.js
+++ commands/slash/giveaway/premium.js
@@ -1,4 +1,5 @@
 const { EmbedBuilder } = require("discord.js");
+const { uuidv4 } = require("../../util/crypto");
 
 module.exports = {
     name: "premium",
@@ -12,12 +13,26 @@ module.exports = {
 
         const number = interaction.options.getInteger("number");
         const type = interaction.options.getString("type");
+        // generate a uuid for the giveaway
+        const giveawayUUID = uuidv4();
+        console.log(giveawayUUID);
 
         const embed = new EmbedBuilder()
             .setTitle("Premium Giveaway")
             .setDescription(`First ${number} people to press the button below will get a ${type} of free premium!`)
             .setFooter(`Requested by ${interaction.user.tag} | Evo V4 Giveaways`, interaction.user.avatarURL())
             .setTimestamp();
+
+        const row = new MessageActionRow().addComponents(
+            new MessageButton().setCustomId("premium").setLabel("Get Premium").setStyle("PRIMARY")
+        );
+
+        const msg = await interaction.editReply({ embeds: [embed], components: [row] });
+
+        const filter = (i) => i.customId === "premium" && i.user.id !== client.user.id;
+        // make an infinitely long collector
+
+        const collector = msg.createMessageComponentCollector({ filter, time: 0 });
 
         interaction.editReply({ content: "Sent" });
     },
