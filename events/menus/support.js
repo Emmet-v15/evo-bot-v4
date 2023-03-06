e711a70f9b74ae127529356193d183157fcc5a25
emmet-v15 <emmet@v15.studio>
2023-03-06 15:31:12 +0000

    penis esp

--- events/menus/support.js
+++ events/menus/support.js
@@ -77,7 +77,7 @@ module.exports = async (
                         .setMinLength(10)
                         .setMaxLength(400)
                         .setRequired(true)
-                        .setPlaceholder("e.g. I would like to see a new feature (penis esp) that ...");
+                        .setPlaceholder("e.g. I suggest (penis esp) to be ...");
 
                     const firstActionRow = new ActionRowBuilder().addComponents(suggestionType).addComponents(suggestionInfo);
 
