bc7e1b74a5a04ac56973db92ba8c507f7d460c1d
emmet-v15 <emmet@v15.studio>
2023-03-06 17:07:32 +0000

    penis esp

--- events/menus/support.js
+++ events/menus/support.js
@@ -51,7 +51,7 @@ module.exports = async (
 
             switch (category) {
                 case "bug": {
-                    interaction.deferReply({ ephemeral: true });
+                    await interaction.deferReply({ ephemeral: true });
                     interaction.editReply({ content: "Not yet implemented.", ephemeral: true });
                     break;
                     const bugModal = new ModalBuilder().setCustomId("tickets-create-bug").setTitle("Create a bug report");
@@ -104,7 +104,7 @@ module.exports = async (
                     return await interaction.showModal(bugModal);
                 }
                 case "suggestion": {
-                    interaction.deferReply({ ephemeral: true });
+                    await interaction.deferReply({ ephemeral: true });
                     interaction.editReply({ content: "Not yet implemented.", ephemeral: true });
                     break;
 
