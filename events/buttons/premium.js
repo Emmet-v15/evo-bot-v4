9bc29f306d5afd0734fb9aa4e4616aca63e37855
emmet-v15 <emmet@v15.studio>
2023-03-07 21:32:23 +0000

    finish giveaway command

--- events/buttons/premium.js
+++ events/buttons/premium.js
@@ -1,6 +1,7 @@
 const { convertToOrdinal } = require("../../util/common");
 const { EmbedBuilder } = require("discord.js");
 const logger = require("../../systems/logging/logger");
+require("dotenv").config();
 
 module.exports = async (
     /** @type {import("discord.js").Client} */ client,
@@ -36,12 +37,51 @@ module.exports = async (
                     embed = new EmbedBuilder()
                         .setTitle("Premium")
                         .setDescription(`You already have ${mappings[type]}!`)
-                        .setFooter({ text: `Giveaways | Evo V4™️`, iconURL: client.user.displayAvatarURL() })
+                        .setFooter({
+                            text: `Giveaways | Evo V4™️ - Make a general ticket if you cannot execute`,
+                            iconURL: client.user.displayAvatarURL(),
+                        })
                         .setColor(role.hexColor)
                         .setTimestamp();
                 } else {
                     member.roles.add(role);
+                    if (type == "beta") member.roles.add(client.settings.get(interaction.guild.id, "premium.role"));
                     logger.event(`User ${interaction.user.tag} [${interaction.user.id}] won giveaway for ${mappings[type]}.`);
+
+                    await fetch(`https://api.luarmor.net/v3/projects/${process.env.LUARMOR_PROJECT_ID}/users`, {
+                        method: "POST",
+                        headers: {
+                            Authorization: process.env.LUARMOR_API_KEY,
+                            "Content-Type": "application/json",
+                        },
+                        body: JSON.stringify({
+                            discord_id: interaction.user.id,
+                            note: "Created by Evo V4™️ Bot",
+                        }),
+                    }).then((res) => {
+                        switch (res.status) {
+                            case 200: {
+                                const json = res.json();
+                                client.userDB.set(interaction.user.id, json.user_key, "luarmorKey");
+                                break;
+                            }
+                            case 400: {
+                                // user already exists
+                                break;
+                            }
+                            case 403: {
+                                throw new Error("Invalid Luarmor API Key");
+                            }
+                            case 429: {
+                                // too many requests
+                                throw new Error("Rate Limited at Luarmor API");
+                            }
+                            default: {
+                                throw new Error("Unknown Error at Luarmor API: ", res.status, res.statusText, res.body, res.url);
+                            }
+                        }
+                    });
+
                     embed = new EmbedBuilder()
                         .setTitle("Premium")
                         .setDescription(
--- test.js
+++ test.js
@@ -1,4 +1,49 @@
-const dateString = "2023-02-10T13:28:04.02Z";
-const date = new Date(dateString);
+require("dotenv").config();
+const fetch = (...args) => import("node-fetch").then(({ default: fetch }) => fetch(...args));
 
-console.log(date.getTime());
+(async () => {
+    // delete
+    const key = "EUFEbGLriYiNkJglwiXSUhBYrngLyDfe";
+    await fetch(`https://api.luarmor.net/v3/projects/${process.env.LUARMOR_PROJECT_ID}/users?user_key=${key}`, {
+        method: "DELETE",
+        headers: {
+            Authorization: process.env.LUARMOR_API_KEY,
+        },
+    })
+        .then((res) => res.json())
+        .then(console.log);
+
+    await fetch(`https://api.luarmor.net/v3/projects/${process.env.LUARMOR_PROJECT_ID}/users`, {
+        method: "POST",
+        headers: {
+            Authorization: process.env.LUARMOR_API_KEY,
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+            discord_id: interaction.user.id,
+            note: "Created by Evo V4™️ Bot",
+        }),
+    }).then((res) => {
+        switch (res.status) {
+            case 200: {
+                const json = res.json();
+                client.userDB.set(interaction.user.id, json.user_key, "luarmorKey");
+                break;
+            }
+            case 400: {
+                // user already exists
+                break;
+            }
+            case 403: {
+                throw new Error("Invalid Luarmor API Key");
+            }
+            case 429: {
+                // too many requests
+                throw new Error("Rate Limited at Luarmor API");
+            }
+            default: {
+                throw new Error("Unknown Error at Luarmor API: ", res.status, res.statusText, res.body, res.url);
+            }
+        }
+    });
+})();
