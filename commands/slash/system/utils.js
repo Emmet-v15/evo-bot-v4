0634dec7acf5b788ab2314f3e714ff59432947b5
emmet-v15 <emmet@v15.studio>
2023-03-08 01:03:30 +0000

    finish giveaway command

---
+++ commands/slash/system/utils.js
@@ -0,0 +1,40 @@
+module.exports = {
+    name: "utils",
+    description: "Utility commands for the bot dev.",
+    permission: 3,
+    execute: async (
+        /** @type {require("discord.js").Client} */ client,
+        /** @type {require("discord.js").CommandInteraction} */ interaction
+    ) => {
+        interaction.deferReply({ ephemeral: true });
+
+        const action = interaction.options.getString("action");
+
+        if (action === "outputsettings") {
+            const settings = client.settings.get(interaction.guild.id);
+            const settingsString = JSON.stringify(settings, null, 4);
+            const settingsFile = new MessageAttachment(Buffer.from(settingsString), "settings.json");
+            interaction.editReply({ files: [settingsFile] });
+        } else if (action === "outputuserdb") {
+            const userDB = client.userDB.get(interaction.guild.id);
+            const userDBString = JSON.stringify(userDB, null, 4);
+            const userDBFile = new MessageAttachment(Buffer.from(userDBString), "userdb.json");
+            interaction.editReply({ files: [userDBFile] });
+        }
+        
+
+        interaction.editReply({ content: "Success!" });
+    },
+    options: [
+        {
+            type: "String",
+            name: "action",
+            description: "The action to perform",
+            choices: [
+                { "OutputSettings": "outputsettings" },
+                { "OutputUserDB": "outputuserdb" },
+            ]
+            required: true,
+        },
+    ],
+};
