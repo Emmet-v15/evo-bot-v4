edb538db6cbcd8e2bdcf145a654dbd961710f9b7
emmet-v15 <emmet@v15.studio>
2023-03-08 01:08:33 +0000

    minor changes

--- events/buttons/premium.js
+++ events/buttons/premium.js
@@ -11,7 +11,7 @@ module.exports = async (
 ) => {
     switch (args[0]) {
         case "access": {
-            interaction.guild.channels.cache.get("1074448895372951592").send(`<@${interaction.user.id}> tried to claim`);
+            // interaction.guild.channels.cache.get("1074448895372951592").send(`<@${interaction.user.id}> tried to claim`);
 
             return;
         }
