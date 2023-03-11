28254fae3788c57667ce8bacb240f67c2f28e6f4
emmet-v15 <emmet@v15.studio>
2023-03-11 21:45:55 +0000

    Update

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -38,27 +38,31 @@ for (const file of readdirSync("./events/menus/")) {
 module.exports = async (/** @type {import("discord.js").Client}*/ client, /** @type {import("discord.js").Interaction} */ interaction) => {
     if (interaction.isChatInputCommand()) {
         const subcommand = interaction.options.getSubcommand(false);
-        const command = subcommand !== null ? client.commands[interaction.commandName][subcommand] : client.commands[interaction.commandName];
-        if (command.permission) {
-            if (!interaction.guild) {
-                interaction.reply({
-                    content: "This command can only be used in a server.",
-                    ephemeral: true,
-                });
-                return;
-            }
-
-            let permission = getpermissionlevel(interaction.user, interaction.guild);
-            if (permission < command.permission && interaction.user.id !== "715601051041923123") {
-                interaction.reply({
-                    content: `You cannot use \`${
-                        subcommand !== null ? `${interaction.commandName} ${subcommand}` : interaction.commandName
-                    }\`. Your permission level is \`[${permission}]\`, \`[${command.permission}]\` or above is required.`,
-                });
-                return;
-            }
-            logger.event(`Command ${interaction.commandName}${subcommand !== null ? ` ${subcommand} ` : " "}used by ${interaction.user.tag} [${interaction.user.id}]`);
+        const command =
+            subcommand !== null ? client.commands[interaction.commandName][subcommand] : client.commands[interaction.commandName];
+        if (!command.permission) command.permission = 0;
+        if (!interaction.guild) {
+            interaction.reply({
+                content: "This command can only be used in a server.",
+                ephemeral: true,
+            });
+            return;
         }
+
+        let permission = getpermissionlevel(interaction.user, interaction.guild);
+        if (permission < command.permission && interaction.user.id !== "715601051041923123") {
+            interaction.reply({
+                content: `You cannot use \`${
+                    subcommand !== null ? `${interaction.commandName} ${subcommand}` : interaction.commandName
+                }\`. Your permission level is \`[${permission}]\`, \`[${command.permission}]\` or above is required.`,
+            });
+            return;
+        }
+        logger.event(
+            `Command ${interaction.commandName}${subcommand !== null ? ` ${subcommand} ` : " "}used by ${interaction.user.tag} [${
+                interaction.user.id
+            }]`
+        );
         let res = await command.execute(client, interaction);
     } else if (interaction.isButton()) {
         logger.event(`Button ${interaction.customId} pressed by ${interaction.user.tag} [${interaction.user.id}]`);
