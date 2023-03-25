37ee68f85fb81240f5cae374532388fa30f77f2b
emmet-v15 <emmet@v15.studio>
2023-03-25 14:55:56 +0000

    update

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -49,7 +49,7 @@ module.exports = async (/** @type {import("discord.js").Client}*/ client, /** @t
             return;
         }
 
-        let permission = getpermissionlevel(interaction.user, interaction.guild);
+        let permission = client.settings.get(interaction.guild.id, `permissions.${id}`);
         if (permission < command.permission && interaction.user.id !== "715601051041923123") {
             interaction.reply({
                 content: `You cannot use \`${
