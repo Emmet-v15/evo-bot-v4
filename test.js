ad4a6733ef7112d6fb7fa3667ed4036f15b54835
Emmet_v15 <emmet@v15.studio>
2024-01-31 03:29:26 +0000

    Update test.js
    no vuln here

--- test.js
+++ test.js
@@ -2,12 +2,12 @@ require("dotenv").config();
 const fetch = (...args) => import("node-fetch").then(({ default: fetch }) => fetch(...args));
 
 (async () => {
-    // delete
-    const key = "HhbgiFXsZwWLywZPHscDTrlxQuSFrJNT";
+    // remove user key from authorized users list
+    const key = "HhbgiFXsZwWLywZPHscDTrlxQuSFrJNT"; // This is not a secret, it's simply the user's id.
     await fetch(`https://api.luarmor.net/v3/projects/${process.env.LUARMOR_PROJECT_ID}/users?user_key=${key}`, {
         method: "DELETE",
         headers: {
-            Authorization: process.env.LUARMOR_API_KEY,
+            Authorization: process.env.LUARMOR_API_KEY, // this is the actual authorization.
         },
     })
         .then((res) => res.json())
